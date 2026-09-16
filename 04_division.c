08040ac6c5898a061dd561295b4254f562d5cd51
jishanusmanshaikh2007-ux <jishanusmanshaikh2007@gmail.com>
2026-09-16 15:44:47 +0530

    Add files via upload

---
+++ 04_division.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float a, b;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+    if (b != 0)
+        printf("Division = %.2f", a / b);
+    else
+        printf("Cannot divide by zero.");
+    return 0;
+}
